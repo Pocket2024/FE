ab985ee1146913e8a7a23226973da7da8e0e9b02
solux <lee.yeji0327@gmail.com>
2024-05-09 23:36:35 +0900

    [add] searchpage, myticketpage 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/LandingPage';
 import TopBar from './components/TopBar';
+import SearchPage from './pages/SearchPage';
+import MyTicketPage from './pages/MyTicketPage';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
     <TopBar/>
     <Routes>
         <Route path="/" element={<LandingPage/>}/>
+        <Route path='/search' element={<SearchPage/>}/>
+        <Route path='/myticket' element={<MyTicketPage/>}/>
     </Routes>
     </>
   );
