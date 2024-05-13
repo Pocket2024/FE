f19ea2a8d063f40bfe1955353199b3480be08a70
solux <lee.yeji0327@gmail.com>
2024-05-13 21:21:51 +0900

    [add] landing3-mobile

--- src/components/Landing3.jsx
+++ src/components/Landing3.jsx
@@ -12,7 +12,7 @@ const Wrapper3 = styled.div`
 const Txt = styled.div`
     color: white;
     font-size: 30px;
-    font-weight: 900;
+    font-weight: 700;
     text-align: center;
     padding-top: 100px;
 `
@@ -68,10 +68,65 @@ const Landing3 = () => {
             </ButtonLine>
         </Wrapper3>
         :
-        <></>
+        <MWrapper3>
+            <MTxt>지금 당장 나만의 티켓북을 만들고 싶다면</MTxt>
+            <MButtonLine>
+                <MButton 
+                    onMouseOver={() => setIsHover(true)}
+                    onMouseOut={() => setIsHover(false)}>
+                    <IoIosArrowRoundForward color={isHover? '#161616' : 'white'} size={25}/>
+                    <div>로그인하기</div>
+                </MButton>
+                <MButton
+                    onMouseOver={() => setIsHover2(true)}
+                    onMouseOut={() => setIsHover2(false)}>
+                    <IoIosArrowRoundForward color={isHover2? '#161616' : 'white'} size={25}/>
+                    <div>회원가입하기</div>
+                </MButton>
+            </MButtonLine>
+        </MWrapper3>
     }
     </>
     );
 };
 
 export default Landing3;
+
+const MWrapper3 = styled.div`
+    height: calc(100vh - 80px);
+    background-color: #E65A2E;
+    position: relative;
+    z-index: 2;
+`
+const MTxt = styled.div`
+    color: white;
+    font-size: 20px;
+    font-weight: 700;
+    text-align: center;
+    padding-top: 100px;
+`
+const MButton = styled.button`
+    display: flex;
+    border: none;
+    background-color: #161616;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 50px 5px;
+    cursor: pointer;
+    &:hover {
+        background-color: white;
+        > div {
+            color: #161616;
+        }
+    }
+    > div {
+        margin-left: 20px;
+        color: white;
+        font-size: 15px;
+        font-weight: 700;
+    }
+`
+const MButtonLine = styled.div`
+    display: flex;
+    justify-content: center;
+`
