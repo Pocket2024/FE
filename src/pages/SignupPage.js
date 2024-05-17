7ec63707a2af3e9ec082816d55eef70d250d4a4e
solux <lee.yeji0327@gmail.com>
2024-05-18 00:18:47 +0900

    [feat] signuppage 틀 생성

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -23,7 +23,21 @@ const SignupPage = () => {
         <MWrapper>
             <Title>회원가입</Title>
             <FormTitle>이메일</FormTitle>
-            <Input />
+            <Input 
+                placeholder='이메일을 입력해주세요.'/>
+            <FormTitle>닉네임</FormTitle>
+            <Input 
+                placeholder='닉네임을 입력해주세요.'/>
+            <FormTitle>비밀번호</FormTitle>
+            <Input 
+                placeholder='비밀번호를 입력해주세요.'/>
+            <FormTitle>비밀번호 확인</FormTitle>
+            <Input 
+                placeholder='비밀번호를 한 번 더 입력해주세요.'/>
+            <FormTitle>한 줄 소개</FormTitle>
+            <Input 
+                placeholder='한 줄로 자신을 소개해주세요.'/>
+            <MSignupBtn>회원가입 완료</MSignupBtn>
         </MWrapper>
         }
         </>
@@ -49,9 +63,32 @@ const FormTitle = styled.div`
     font-size: 15px;
     font-weight: 600;
     color: #E65A2E;
+    margin-bottom: 5px;
+    margin-top: 15px;
 `
 const Input = styled.input`
     border: 1px solid rgba(255, 149, 115, 0.44);
     outline: none;
     background-color: #FFF6F3;
+    border-radius: 10px;
+    width: 100%;
+    padding: 15px 20px;
+    font-size: 12px;
+    font-weight: 700;
+    color: #E65A2E;
+    &::placeholder {
+        color: rgba(230, 90, 46, 0.53);
+        font-size: 12px;
+    }
+`
+const MSignupBtn = styled.button`
+    border: none;
+    background-color: #E65A2E;
+    border-radius: 40px;
+    color: white;
+    font-size: ${props => props.font};
+    font-weight: 600;
+    padding: 20px;
+    width: 100%;
+    margin-top: 50px;
 `
