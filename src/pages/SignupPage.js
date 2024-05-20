90bf06402eb94c33041628885c5174cc0412ad73
solux <lee.yeji0327@gmail.com>
2024-05-20 18:53:55 +0900

    [feat] signuppage-desktop

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -7,7 +7,18 @@ const Wrapper = styled.div`
     width: 100vw;
     height: calc(100vh - 80px);
     background-color: white;
-    padding: 0 30vw;
+    padding: 0 5vw;
+    display: flex;
+    justify-content: space-between;
+`
+const Left = styled.div`
+    width: 43vw;
+    background-color: aliceblue;
+`
+const Right = styled.div`
+    width: 43vw;
+    padding: 50px;
+    margin-bottom: 100px;
 `
 
 const SignupPage = () => {
@@ -17,11 +28,42 @@ const SignupPage = () => {
         <>
         {isDesktop?
         <Wrapper>
-
+            <Left>
+                f
+            </Left>
+            <Right>
+                <Title fontsize='35px'>회원가입</Title>
+                <FormTitle fontsize='25px' mbottom='10px'>이메일</FormTitle>
+                <Input 
+                    fontsize='20px'
+                    padding='20px 25px'
+                    placeholder='이메일을 입력해주세요.'/>
+                <FormTitle fontsize='25px' mbottom='10px' mtop='30px'>닉네임</FormTitle>
+                <Input 
+                    fontsize='20px'
+                    padding='20px 25px'
+                    placeholder='닉네임을 입력해주세요.'/>
+                <FormTitle fontsize='25px' mbottom='10px' mtop='30px'>비밀번호</FormTitle>
+                <Input 
+                    fontsize='20px'
+                    padding='20px 25px'
+                    placeholder='비밀번호를 입력해주세요.'/>
+                <FormTitle fontsize='25px' mbottom='10px' mtop='30px'>비밀번호 확인</FormTitle>
+                <Input 
+                    fontsize='20px'
+                    padding='20px 25px'
+                    placeholder='비밀번호를 한 번 더 입력해주세요.'/>
+                <FormTitle fontsize='25px' mbottom='10px' mtop='30px'>한 줄 소개</FormTitle>
+                <Input 
+                    fontsize='20px'
+                    padding='20px 25px'
+                    placeholder='한 줄로 자신을 소개해주세요.'/>
+                <MSignupBtn fontsize='20px'>회원가입 완료</MSignupBtn>
+            </Right>
         </Wrapper>
         :
         <MWrapper>
-            <Title>회원가입</Title>
+            <Title fontsize='25px'>회원가입</Title>
             <FormTitle>이메일</FormTitle>
             <Input 
                 placeholder='이메일을 입력해주세요.'/>
@@ -53,18 +95,18 @@ const MWrapper = styled.div`
     padding: 0 10vw;
 `
 const Title = styled.div`
-    font-size: 25px;
+    font-size: ${props => props.fontsize};
     font-weight: 700;
     color: #E65A2E;
     margin-bottom: 20px;
     margin-top: 30px;
 `
 const FormTitle = styled.div`
-    font-size: 15px;
+    font-size: ${props => props.fontsize || '15px'};
     font-weight: 600;
     color: #E65A2E;
-    margin-bottom: 5px;
-    margin-top: 15px;
+    margin-bottom: ${props => props.mbottom || '5px'};
+    margin-top: ${props => props.mtop || '15px'};
 `
 const Input = styled.input`
     border: 1px solid rgba(255, 149, 115, 0.44);
@@ -72,13 +114,13 @@ const Input = styled.input`
     background-color: #FFF6F3;
     border-radius: 10px;
     width: 100%;
-    padding: 15px 20px;
-    font-size: 12px;
+    padding: ${props => props.padding || '15px 20px'};
+    font-size: ${props => props.fontsize || '12px'};
     font-weight: 700;
     color: #E65A2E;
     &::placeholder {
         color: rgba(230, 90, 46, 0.53);
-        font-size: 12px;
+        font-size: ${props => props.fontsize || '12px'};
     }
 `
 const MSignupBtn = styled.button`
@@ -86,7 +128,7 @@ const MSignupBtn = styled.button`
     background-color: #E65A2E;
     border-radius: 40px;
     color: white;
-    font-size: ${props => props.font};
+    font-size: ${props => props.fontsize};
     font-weight: 600;
     padding: 20px;
     width: 100%;
