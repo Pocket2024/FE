f1f00c345e1d37c2a65b0ff8a073304bb50abaec
solux <lee.yeji0327@gmail.com>
2024-05-29 18:37:41 +0900

    [add] login 연동 코드

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import {useMediaQuery} from 'react-responsive';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Wrapper = styled.div`
     width: 100vw;
@@ -22,7 +23,18 @@ const LoginPage = () => {
         setPw(e.target.value);
     }
     const handleLogin = () => {
-
+        axios.post('http://127.0.0.1:8000/api/users/login', {
+            email: email,
+            password: pw
+        })
+        .then(res => {
+            console.log(res);
+            alert('로그인 성공');
+            navigate('/');
+        })
+        .catch(err => {
+            console.log('Error handle login', err);
+        })
     }
     return (
         <>
@@ -50,6 +62,8 @@ const LoginPage = () => {
             <MInput 
                 onChange={onChangePw}
                 placeholder='비밀번호를 입력해주세요.' font='12px' padding='15px 20px'/>
+            <LoginBtn font='15px'
+                onClick={handleLogin}>로그인</LoginBtn>
             <MTxt size='12px' mbottom='10px' ptop='5vh'>계정이 없으신가요?</MTxt>
             <MSignupBtn font='15px'
                 onClick={() => {navigate('/signup')}}>회원가입하기</MSignupBtn>
