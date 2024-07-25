2e70779d81be4f7bee90764ecec1ea0411209975
solux <lee.yeji0327@gmail.com>
2024-07-25 23:37:13 +0900

    [add] detailpage + detail

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -2,7 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import Detail from "../components/Detail";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  background-color: #262626;
+  width: 100vw;
+  min-height: calc(100vh - 80px);
+  padding: 0 30px;
+`;
 
 const DetailPage = () => {
   return (
