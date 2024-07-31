6a6fe0d55e51e822e91b6999159acc5aba34c4b3
solux <lee.yeji0327@gmail.com>
2024-07-31 22:14:55 +0900

    [feat] searchpage search input 구현

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { IoSearch } from "react-icons/io5";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  min-height: calc(100vw - 80px);
+  background-color: #262626;
+  padding: 50px 100px;
+`;
+const SearchTitle = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+  color: white;
+`;
+const SearchDiv = styled.div`
+  margin-top: 20px;
+  width: 100%;
+  padding: 20px 40px;
+  background-color: #3d3d3d;
+  display: flex;
+  align-items: center;
+  border-radius: 50px;
+  gap: 0 15px;
+`;
+const SearchInput = styled.input`
+  background-color: #3d3d3d;
+  border: none;
+  outline: none;
+  width: 100%;
+  font-size: 20px;
+  color: white;
+  font-weight: 500;
+`;
 
 const SearchPage = () => {
-    return (
-        <div>
-            탐색 페이지
-        </div>
-    );
+  const [keyword, setKeyword] = useState("");
+  const onChangeSearch = (e) => setKeyword(e.target.value);
+  return (
+    <Wrapper>
+      <SearchTitle>키워드 검색을 통한 티켓 탐색 🔍</SearchTitle>
+      <SearchDiv>
+        <IoSearch color="white" size={30} />
+        <SearchInput
+          placeholder="키워드를 입력해주세요"
+          value={keyword}
+          onChange={onChangeSearch}
+        />
+      </SearchDiv>
+    </Wrapper>
+  );
 };
 
 export default SearchPage;
