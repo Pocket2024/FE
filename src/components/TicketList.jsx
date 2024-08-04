f733294fd31384774383bb26d2a6015a88f55e97
solux <lee.yeji0327@gmail.com>
2024-08-04 17:31:15 +0900

    [feat] ticketlist 컴포넌트 구현

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import styled from "styled-components";
+import dummy from "../dummy/hot.json";
+import Ticket from "./Ticket";
 
 const List = styled.div`
-  color: white;
+  display: block;
+  margin-top: 30px;
+`;
+const TicketDiv = styled.div`
+  margin-bottom: 30px;
 `;
 
 const TicketList = () => {
-  return <List>티켓리스트</List>;
+  return (
+    <List>
+      {dummy.data.map((ticket) => (
+        <>
+          <TicketDiv>
+            <Ticket
+              key={ticket.id}
+              title={ticket.title}
+              place={ticket.place}
+              seat={ticket.seat}
+              year={ticket.year}
+              date={ticket.date}
+              custom={ticket.customimg}
+            />
+          </TicketDiv>
+        </>
+      ))}
+    </List>
+  );
 };
 
 export default TicketList;
