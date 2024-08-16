7a5e18d0d1c90df3594a56e92021a8fa71fe7319
solux <lee.yeji0327@gmail.com>
2024-08-16 19:52:54 +0900

    [del] responsive context 불필요 코드 삭제

--- src/context/Responsive.js
+++ src/context/Responsive.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext } from "react";
 import { useMediaQuery } from "react-responsive";
 
 const ResponsiveContext = createContext();
