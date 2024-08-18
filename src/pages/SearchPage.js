d863240dc65088e2a3e7bef6f31432b1f7c50cf9
solux <lee.yeji0327@gmail.com>
2024-08-19 00:05:00 +0900

    [feat] 검색 api 연동

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { IoSearch } from "react-icons/io5";
 import HotTicket from "../components/HotTicket";
 import RecentTicket from "../components/RecentTicket";
+import api from "../api/api";
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -34,19 +35,104 @@ const SearchInput = styled.input`
   color: white;
   font-weight: 500;
 `;
+const TypeRadioBox = styled.div`
+  margin-top: 20px;
+  display: flex;
+  gap: 0 10px;
+`;
+const TypeInput = styled.input`
+  display: none;
+  &:checked + label {
+    background-color: #ffe976; /* 체크된 경우 배경색 변경 */
+    span {
+      color: #151515;
+    }
+  }
+`;
+const TypeLabel = styled.label`
+  padding: 8px 25px;
+  background-color: #656565;
+  border-radius: 20px;
+  cursor: pointer;
+  &:hover {
+    background-color: #8a8a8a;
+  }
+`;
+const TypeSpan = styled.span`
+  color: white;
+  font-size: 17px;
+  font-weight: 500;
+`;
 
 const SearchPage = () => {
+  let ACCESS_TOKEN = localStorage.getItem("accessToken");
   const [keyword, setKeyword] = useState("");
+  const [selectedTypeId, setSelectedTypeId] = useState(1); // 기본값을 1로 설정
+  const [selectedType, setSelectedType] = useState("title");
+  const [isSearch, setIsSearch] = useState(false);
+  const [searchResult, setSearchResult] = useState([]);
   const onChangeSearch = (e) => setKeyword(e.target.value);
+  const handleTypeChange = (id, type) => {
+    setSelectedTypeId(id); // 선택된 타입을 업데이트
+    setSelectedType(type);
+  };
+  const searchType = [
+    { id: 1, value: "title", name: "제목" },
+    { id: 2, value: "location", name: "장소" },
+    { id: 3, value: "seat", name: "좌석" },
+    { id: 4, value: "date", name: "날짜" },
+    { id: 5, value: "content", name: "내용" },
+  ];
+  const handleSearch = () => {
+    api
+      .get(
+        `/api/reviews/search?keyword=${keyword}&searchType=${selectedType}`,
+        {
+          headers: {
+            Authorization: `${ACCESS_TOKEN}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        setSearchResult(res.data);
+        setIsSearch(true);
+      })
+      .catch((err) => {
+        console.log("검색 에러", err);
+      });
+  };
+  const activeEnter = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
   return (
     <Wrapper>
       <SearchTitle>키워드 검색을 통한 티켓 탐색 🔍</SearchTitle>
+      <TypeRadioBox>
+        {searchType.map((type) => (
+          <>
+            <TypeInput
+              type="radio"
+              id={type.id}
+              value={type.value}
+              checked={selectedTypeId === type.id} // 선택된 타입을 확인
+              onChange={() => handleTypeChange(type.id, type.value)}
+            />
+            <TypeLabel htmlFor={type.id} key={type.id}>
+              <TypeSpan>{type.name}</TypeSpan>
+            </TypeLabel>
+          </>
+        ))}
+      </TypeRadioBox>
       <SearchDiv>
         <IoSearch color="white" size={30} />
         <SearchInput
           placeholder="키워드를 입력해주세요"
           value={keyword}
           onChange={onChangeSearch}
+          onKeyDown={(e) => activeEnter(e)}
         />
       </SearchDiv>
       <HotTicket />
