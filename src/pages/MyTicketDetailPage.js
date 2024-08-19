09e1749d4d105cc5160693d4b119ddac764d919f
solux <lee.yeji0327@gmail.com>
2024-08-19 13:04:47 +0900

    [refactor] myticketdetailpage warning 제거

--- src/pages/MyTicketDetailPage.js
+++ src/pages/MyTicketDetailPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Detail from "../components/Detail";
 import UploadBtn from "../components/UploadBtn";
