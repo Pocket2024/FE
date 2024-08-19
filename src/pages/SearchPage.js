e8b1621876960437abb9f414b8ca9dd3f37649b0
solux <lee.yeji0327@gmail.com>
2024-08-19 13:03:00 +0900

    [refactor] searchpage warning 제거

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { IoSearch } from "react-icons/io5";
 import HotTicket from "../components/HotTicket";
