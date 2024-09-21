bfc97df3b3a1fce1ba437a7d38477f6eac0cf3ee
solux <lee.yeji0327@gmail.com>
2024-09-21 16:14:10 +0900

    [chore] loading 컴포넌트 css 수정

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -6,10 +6,14 @@ import { useResponsive } from "../context/Responsive";
 const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 99999;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #262626;
+  background-color: #00000086;
 `;
 const Image = styled.img`
   width: ${(props) => props.width};
